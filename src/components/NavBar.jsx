af9edd844ff3818abb16a7ca168d9582cfc27878
Max C <smashmouthmax@gmail.com>
2023-06-03 16:32:11 -0700

    added style sheet for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useAuth } from "../contexts/AuthContext";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import styles from "./NavBarStyle.module.css";
 
 export default function NavBar() {
   const [error, setError] = useState();
@@ -22,20 +23,26 @@ export default function NavBar() {
   }
 
   return (
-    <nav>
-      <h1>Buzzy Bets</h1>
-      <ul>
+    <nav className={styles.navbar}>
+      <h1 className={styles.title}>Buzzy Bets</h1>
+      <ul className={styles.links}>
         {currentUser ? (
           <>
             <button type="button" onClick={handleLogOut}>
               Log Out
             </button>
-            <Link href="/update-profile">Update Profile</Link>
+            <Link className={styles.link} href="/update-profile">
+              Update Profile
+            </Link>
           </>
         ) : (
           <>
-            <Link href="/signup">Sign Up</Link>
-            <Link href="/login">Log In</Link>
+            <Link className={styles.link} href="/signup">
+              Sign Up
+            </Link>
+            <Link className={styles.link} href="/login">
+              Log In
+            </Link>
           </>
         )}
       </ul>
